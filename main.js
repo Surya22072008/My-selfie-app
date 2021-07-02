6c762ee4ef5379bcbc513d5dd58bed9dd32b7e8a
Surya22072008 <78208952+Surya22072008@users.noreply.github.com>
2021-07-02 14:31:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,16 +16,28 @@ recognition.onresult = function(event) {
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
+    if(Content == "take my selfie") {
+        console.log("taking selfie ---");
         speak();
+    }
+     
 }
-
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
 
 function speak() {
 var synth = window.speechSynthesis;
-speak_data = document.getElementById("textbox").value;
+speak_data = "Taking your Selfie in 5 seconds";
 var utterThis = new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout(function() {
+take_snapshot();save();
+},5000);
  }
  Webcam.set({
     width:360,
@@ -34,4 +46,9 @@ image_format: 'png',
 png_quality:90
 });
 camera = document.getElementById("camera");
+function take_snapshot() {
+    Webcam.snap(function(data_url){
+document.getElementById("results").innerHTML = '<img id="selfie_image" src="' + data_url+'">';
+    });
+}
  
